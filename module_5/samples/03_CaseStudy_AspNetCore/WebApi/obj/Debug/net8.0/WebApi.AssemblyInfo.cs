827166d3de51aa316d03a220631495aaa37d059d
wanshazamirul <shazwan.amirul@gmail.com>
2026-09-09 13:01:23 +0800

    03_case study aspnet core

--- module_5/samples/03_CaseStudy_AspNetCore/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ module_5/samples/03_CaseStudy_AspNetCore/WebApi/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6816c549a45362bdd75d69f1ed2261a5d5d0924f")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
